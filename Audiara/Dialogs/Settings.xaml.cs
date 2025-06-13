5557a4c0423cd110c2ecf9720792ebd195e8c2a4
Jeniear <135406459+Jienniers@users.noreply.github.com>
2025-06-13 19:05:43 +0500

    [Cleanup] Add TODO comment to clarify unused Settings dialog reserved for future use

--- Audiara/Dialogs/Settings.xaml.cs
+++ Audiara/Dialogs/Settings.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.IO;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace Audiara.Dialogs
 {
     /// <summary>
     /// Interaction logic for Settings.xaml
+    /// 
+    /// TODO: This Settings window is not currently used in the application.
+    /// It is kept as a placeholder for future implementation of app configuration options.
     /// </summary>
     public partial class Settings : Window
     {
